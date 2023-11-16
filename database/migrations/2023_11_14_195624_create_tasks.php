51f0066e0dac0c5ec2305dd67a86116147b54db0
André de Paula Terceiro <andre@terceiro.com.br>
2023-11-16 12:59:30 -0300

    Changing a migration. Ok, this is not right in a perfect environment, but I have only 1 non automatic table (automatic I say a table like 'schema migrations') and I prefered drop all tables, change a migration and run 'php artisan migrate' again

--- database/migrations/2023_11_14_195624_create_tasks.php
+++ database/migrations/2023_11_14_195624_create_tasks.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->text('name');
             $table->boolean('checked');            
             $table->timestamps();
